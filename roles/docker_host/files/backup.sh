f1a21fd1deb89bd8907c8128bfa2f907c0db03ef
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2017-04-05 16:11:55 +0300

    use --clean and pg_dump instead of pg_dumpall

--- roles/docker_host/files/backup.sh
+++ roles/docker_host/files/backup.sh
@@ -1,4 +1,4 @@
 export REV=$(cd ~/pebbles/ && git rev-parse HEAD)
 export VERSION=dump_`date +%d-%m-%Y"_"%H_%M_%S`_$REV.sql
 mkdir -p {{ backup_dir }}
-sudo docker exec -t db pg_dumpall -c -U postgres > /var/lib/pb/backup/$VERSION
+sudo docker exec -t db pg_dump --clean -d pebbles -U postgres | gzip -c > /var/lib/pb/backup/$VERSION.gz
