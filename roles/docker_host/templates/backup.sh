36016589890075fc193404c988b3657327c41d9b
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2017-04-06 12:09:46 +0300

    backup.sh as file, not template

--- roles/docker_host/templates/backup.sh
+++
@@ -1,8 +0,0 @@
-# Backs up into a gzipped postgresql dump
-# dump name contains creation timestamp for orderin
-# and current pebbles commit so restore can be done to the same point in time
-# restores are expected to be manual
-export REV=$(cd ~/pebbles/ && git rev-parse HEAD)
-export VERSION=dump_`date +%d-%m-%Y"_"%H_%M_%S`_$REV.sql
-mkdir -p {{ backup_dir }}
-sudo docker exec -t db pg_dumpall -c -U postgres | gzip -c > {{backup_dir}}$VERSION.gz
