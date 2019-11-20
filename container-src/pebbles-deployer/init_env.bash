af9bb1fe03b173850d73cfc483c88dfe7ad71c22
tourunen <olli.tourunen@csc.fi>
2019-11-20 13:13:57 +0200

    ci repo initialization + cleanups
    Incorporate common pebbles gitlab-ci pipeline setup code to
    init_env.bash to avoid repetition on jobs that use the deployment
    container.
    
    Also fix bash issues pointed out by linter.

--- container-src/pebbles-deployer/init_env.bash
+++ container-src/pebbles-deployer/init_env.bash
@@ -2,7 +2,7 @@
 
 set -e
 
-env_name=$ENV_NAME
+env_name=${ENV_NAME}
 
 echo "Initializing environment for $env_name"
 echo
@@ -11,7 +11,7 @@ if [[ ! -e /dev/shm/secret/vaultpass ]]; then
     mkdir -p /dev/shm/secret/
     touch /dev/shm/secret/vaultpass
     chmod 600 /dev/shm/secret/vaultpass
-    if [ -z $VAULT_PASS ]; then
+    if [[ -z ${VAULT_PASS} ]]; then
         read -s -p "vault password: " VAULT_PASS
         echo
     fi
@@ -24,12 +24,34 @@ export ANSIBLE_INVENTORY=$HOME/pebbles-environments/$env_name
 echo "ANSIBLE_INVENTORY set to $ANSIBLE_INVENTORY"
 echo
 
+# initialize repositories if we are running in a CI pipeline
+if [[ ! -z CI_COMMIT_REF_NAME ]]; then
+    echo 'CI initialization starts'
+    pushd . > /dev/null
+    # copy source in place
+    cp -r ${CI_PROJECT_DIR} /opt/deployment/pebbles
+    # checkout pebbles-environments. match branch name if it exists
+    cd /opt/deployment
+    git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.csc.fi/pebbles/pebbles-environments.git
+    cd /opt/deployment/pebbles-environments
+    git checkout -b ${CI_COMMIT_REF_NAME} -t origin/${CI_COMMIT_REF_NAME} || true
+    git pull
+    # checkout pebbles-deploy. match branch name if it exists
+    cd /opt/deployment
+    git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.csc.fi/pebbles/pebbles-deploy.git
+    cd /opt/deployment/pebbles-deploy
+    git checkout -b ${CI_COMMIT_REF_NAME} -t origin/${CI_COMMIT_REF_NAME} || true
+    git pull
+    popd > /dev/null
+    echo 'CI container initialization done'
+fi
+
 pushd /opt/deployment/pebbles-deploy/playbooks > /dev/null
 
 if [[ -e requirements.yml ]]; then
-  echo "Installing galaxy-roles"
-  echo
-  ansible-galaxy install -f -p $HOME/galaxy-roles -r requirements.yml
+    echo "Installing galaxy-roles"
+    echo
+    ansible-galaxy install -f -p $HOME/galaxy-roles -r requirements.yml
 fi
 
 echo "Initializing ramdisk contents"
@@ -37,14 +59,14 @@ echo
 SKIP_DYNAMIC_INVENTORY=1 ansible-playbook initialize_ramdisk.yml
 echo
 
-if [[ -e /dev/shm/$env_name/openrc.sh ]]; then
-  echo "Sourcing OpenStack credentials"
-  source /dev/shm/$env_name/openrc.sh
+if [[ -e /dev/shm/${env_name}/openrc.sh ]]; then
+    echo "Sourcing OpenStack credentials"
+    source /dev/shm/${env_name}/openrc.sh
 fi
 
-if [[ -e /dev/shm/$env_name/deployment_data.sh ]]; then
-  echo "Sourcing deployment data"
-  source /dev/shm/$env_name/deployment_data.sh
+if [[ -e /dev/shm/${env_name}/deployment_data.sh ]]; then
+    echo "Sourcing deployment data"
+    source /dev/shm/${env_name}/deployment_data.sh
 fi
 
 popd > /dev/null
