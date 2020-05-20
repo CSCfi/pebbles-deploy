9e73e1a9134cc1cd27aaf4338f1d12cf3a0002a3
tourunen <olli.tourunen@csc.fi>
2020-05-20 09:31:18 +0300

    support static inventory
    Static inventory is useful to cut down deployment dependencies for
    Ansible dynamic inventory plugin. It also makes it possible to limit
    the hosts returned by the inventory, which is not possible using the
    built in OpenStack plugin in Ansible 2.9 [1]
    
    - add static IPs based on network prefix for jump and server hosts
    - change main host postfix from 'pebbles' to 'server'
    - export public hostname in deployment container environment for
      scripting
    - add an option to force Heat stack update
    - fix restarting apache 2 in sso container handlers
    
    [1] https://github.com/ansible/ansible/pull/51401
    
    support static inventory
    
    Static inventory is useful to cut down deployment dependencies for
    Ansible dynamic inventory plugin. It also makes it possible to limit
    the hosts returned by the inventory, which is not possible using the
    built in OpenStack plugin in Ansible 2.9 [1]
    
    - add static IPs based on network prefix for jump and server hosts
    - change main host postfix from 'pebbles' to 'server'
    - export public hostname in deployment container environment for
      scripting
    - add an option to force Heat stack update
    - fix restarting apache 2 in sso container handlers
    - add dcterm.bash to help launching new DCs and shells within the
      existing ones
    - remove the option to assign random floating IPs
    
    [1] https://github.com/ansible/ansible/pull/51401

---
+++ scripts/dcterm.bash
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+#
+# This script acts as a wrapper around run_deployment_container, and opens
+# a session for an environment either in a new or existing deployment container.
+# The name of the environment is extracted from active tmux session name or from
+# the first command line argument.
+#
+
+# figure out where we live to find run_deployment_container.bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# try to extract session name from tmux
+if [[ ! -z $TMUX ]]; then
+    env_name=$(tmux display-message -p '#S')
+fi
+
+# fallback: extract environment name from args
+if [[ -z $env_name ]]; then
+    env_name=$1
+fi
+
+# check if we have the environment name figured out
+if [[ -z $env_name ]]; then
+    echo "unable to extract environment name from tmux or shell arguments"
+    exit 1
+fi
+
+# check if a deployment container is already running
+if docker ps | grep -q ${env_name}-deployer; then
+    echo
+    echo "Launching a new shell in existing container '${env_name}-deployer'"
+    echo
+    docker exec -it ${env_name}-deployer bash
+else
+    # Check the OS to find the vaultpass path
+    OS="$(uname -s)"
+
+    case "${OS}" in
+        Darwin*)
+            ramdisk='/Volumes/rRAMDisk'
+            ;;
+        Linux*)
+            ramdisk='/dev/shm'
+            ;;
+        *)
+            echo "Only Darwin and Linux supported"
+            exit 1
+            ;;
+    esac
+    echo
+    echo "Launching a new deployment container '${env_name}-deployer'"
+    if [[ -e "${ramdisk}/secret/vaultpass-${env_name}" ]]; then
+        vaultpass_path="${ramdisk}/secret/vaultpass-${env_name}"
+        echo "    using environment specific vaultpass-${env_name}"
+    else
+        vaultpass_path="${ramdisk}/secret/vaultpass"
+    fi
+    echo
+
+    ${script_dir}/run_deployment_container.bash -p $vaultpass_path -e $env_name
+fi
