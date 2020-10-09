45c021501233c6b59bf4d83ba030305edd6df494
tourunen <olli.tourunen@csc.fi>
2020-10-09 17:48:03 +0300

    support initializing with data from inventory
    Changes:
    
    bashrc and deploy_functions.bash
    - add helper function refresh-ramdisk
    - add helper function initialize-pebbles-with-initial-data
    - extract waiting functionality to separate functions
    - extract helm installation into its own function
    - make install-pebbles use the initial data from inventory
    - improve comments and help texts
    
    roles/pebbles_deployer/tasks/main.yml
    - extract initial data from inventory to ramdisk

--- container-src/pebbles-deployer/deploy_functions.bash
+++ container-src/pebbles-deployer/deploy_functions.bash
@@ -1,3 +1,10 @@
+# Function for recreating files on ramdisk from ansible-inventory.
+# Useful when developing Helm charts when generated values.yaml needs to be updated after inventory changes.
+refresh-ramdisk() {
+  (cd /opt/deployment/pebbles-deploy && ansible-playbook playbooks/initialize_ramdisk.yml)
+}
+
+# builds logstash in the current OpenShift namespace
 build-image-logstash() {
     # create buildconfig and imagestream if missing
     oc get buildconfig logstash || oc create -f ~/pebbles-deploy/openshift/logstash-bc.yaml
@@ -7,6 +14,7 @@ build-image-logstash() {
     oc start-build logstash --from-dir ~/pebbles-deploy --follow
 }
 
+# builds filebeat in the current OpenShift namespace
 build-image-filebeat() {
     # create buildconfig and imagestream if missing
     oc get buildconfig filebeat || oc create -f ~/pebbles-deploy/openshift/filebeat-bc.yaml
@@ -16,6 +24,7 @@ build-image-filebeat() {
     oc start-build filebeat --from-dir ~/pebbles-deploy --follow
 }
 
+# builds pebbles in the current OpenShift namespace
 build-image-pebbles() {
     # create buildconfig and imagestream if missing
     oc get buildconfig pebbles || oc create -f ~/pebbles-deploy/openshift/pebbles-bc.yaml
@@ -25,6 +34,7 @@ build-image-pebbles() {
     oc start-build pebbles --from-dir ~/pebbles --follow
 }
 
+# builds pebbles-frontend in the current OpenShift namespace
 build-image-pebbles-frontend() {
     # create buildconfig and imagestream if missing
     oc get buildconfig pebbles-frontend || oc create -f ~/pebbles-deploy/openshift/pebbles-frontend-bc.yaml
@@ -42,6 +52,7 @@ build-image-pebbles-frontend() {
     oc start-build pebbles-frontend --from-dir /tmp/pebbles-frontend --follow
 }
 
+# builds all images from local sources
 build-image-all() {
     build-image-pebbles
     build-image-pebbles-frontend
@@ -49,15 +60,17 @@ build-image-all() {
     build-image-filebeat
 }
 
-initialize-pebbles() {
+# blocks until API pod is ready
+wait-for-api-readiness() {
     while echo 'wait for api readiness'; do
         # we grep for readiness for deployments with a single container or with a logging sidecar container
         oc get pod -l name=api | egrep '1/1|2/2' | grep Running && break
         sleep 5
     done
+}
 
-    oc rsh deployment/api python manage.py initialize_system -e admin@example.org -p $1
-
+# blocks until worker-0 pod is ready
+wait-for-worker-readiness() {
     while echo 'wait for worker-0 readiness'; do
         # we grep for readiness for deployments with a single container or with a logging sidecar container
         oc get pod worker-0 | egrep '1/1|2/2' | grep Running && break
@@ -65,17 +78,46 @@ initialize-pebbles() {
     done
 }
 
-install-pebbles() {
+# initializes system with given admin password
+initialize-pebbles() {
     if [[ -z $1 ]]; then
-        echo 'install-pebbles needs admin password as an argument' > /dev/stderr
+        echo 'initialize-pebbles needs admin password as an argument' > /dev/stderr
         return
+    fi
+    wait-for-api-readiness
+    oc rsh deployment/api python manage.py initialize_system -e admin@example.org -p $1
+}
+
+# initializes system with initial data from inventory
+initialize-pebbles-with-initial-data() {
+    wait-for-api-readiness
+    # create database structure
+    oc rsh deployment/api python manage.py create_database
+    # load initial data
+    oc rsh deployment/api python manage.py load_test_data /dev/stdin < /dev/shm/$ENV_NAME/initial_data.yaml
+    # reset worker password to default secret
+    oc rsh deployment/api python manage.py reset_worker_password
+}
+
+# installs system using Helm
+helm-install-pebbles() {
+   (cd ~/pebbles-deploy && helm install pebbles helm_charts/pebbles -f /dev/shm/$ENV_NAME/values.yaml --set overrideSecret=1)
+}
+
+# Builds, installs and initializes system. Uses local source directories and initial data from inventory
+install-pebbles() {
+    if [[ ! -f /dev/shm/$ENV_NAME/initial_data.yaml ]]; then
+      echo "No initial data for this environment found. The manual steps are"
+      echo
+      echo " build-image-all && helm-install-pebbles && initialize-pebbles <admin password>"
+      echo
+      echo "Take a look at ~/deploy_functions.bash to see what is going on under the hood"
+      return 1
     fi
 
     build-image-all
 
-    helm status pebbles 2>&1 > /dev/null ||
-     (cd ~/pebbles-deploy &&
-      helm install pebbles helm_charts/pebbles -f /dev/shm/$ENV_NAME/values.yaml --set overrideSecret=1)
+    helm status pebbles 2>&1 > /dev/null || helm-install-pebbles
 
-    initialize-pebbles $1
+    initialize-pebbles-with-initial-data
 }
