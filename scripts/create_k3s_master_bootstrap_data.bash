deb41fde1120bf9760d721522793685c949d8211
tourunen <olli.tourunen@csc.fi>
2021-04-16 09:17:41 +0300

    k3s: master bootstrap data
    Support provisioning with prepopulated /var/lib/rancher. Useful for
    development environments where you likely want to deploy K3s from
    scratch but avoid changing the cluster credentials in your Pebbles
    deployments all the time.

---
+++ scripts/create_k3s_master_bootstrap_data.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# This script will create an Ansible vault file for master bootstrap data.
+# It should be run in the deployment container against an existing K3s deployment
+
+set -e
+
+OUTPUT_FILE=/tmp/vault_master_bootstrap.yml
+
+# temp file for encoded tarball
+enc_file=$(mktemp)
+
+# create an encoded tar archive of k3s server cred and tls directories
+ssh $ENV_NAME-master sudo tar cvz -C / var/lib/rancher/k3s/server/{cred,tls} |
+  base64 >$enc_file
+
+# construct vaulted file
+vault_file=$(mktemp)
+echo '---' >$vault_file
+echo 'vaulted_master_bootstrap_data: |' >>$vault_file
+sed -e 's/^/  /g' $enc_file >>$vault_file
+
+# encrypt vault
+ansible-vault encrypt $vault_file --output $OUTPUT_FILE
+
+# remove temporary files
+rm -f $enc_file $vault_file
+
+echo
+echo "master bootstrap data vault file was saved to $OUTPUT_FILE"
+echo
