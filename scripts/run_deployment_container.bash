837b3e3fb9fd5ceadf52bda39f3984de80c7ea9e
tourunen <olli.tourunen@csc.fi>
2021-05-28 18:14:53 +0300

    playbook to get k3s certificate
    - add a playbook to get a K3s default certificate from Let's Encrypt and
    place that in the inventory for reuse.
    - add ssh config for master public ip, used here for temporary VM
    - make mounted repositories read-write in deployment container

--- scripts/run_deployment_container.bash
+++ scripts/run_deployment_container.bash
@@ -62,10 +62,10 @@ done
 shift "$((OPTIND-1))"
 
 docker run --rm \
-    -v $SCRIPT_DIR/../../pebbles:/opt/deployment/pebbles:ro \
-    -v $SCRIPT_DIR/../../pebbles-environments:/opt/deployment/pebbles-environments:ro \
-    -v $SCRIPT_DIR/../../pebbles-deploy:/opt/deployment/pebbles-deploy:ro \
-    -v $SCRIPT_DIR/../../pebbles-frontend:/opt/deployment/pebbles-frontend:ro \
+    -v $SCRIPT_DIR/../../pebbles:/opt/deployment/pebbles:rw \
+    -v $SCRIPT_DIR/../../pebbles-environments:/opt/deployment/pebbles-environments:rw \
+    -v $SCRIPT_DIR/../../pebbles-deploy:/opt/deployment/pebbles-deploy:rw \
+    -v $SCRIPT_DIR/../../pebbles-frontend:/opt/deployment/pebbles-frontend:rw \
     --name ${env_name}-deployer \
     $docker_opts \
     $container_image $*
