86601936182f25be4f7b336d720e63d1222ced64
Olli Tourunen <tourunen@csc.fi>
2026-04-07 17:19:41 +0300

    Add support for taking pebbles-deploy source from local tarball
    For local development builds, modify `build.bash` to create a tarball of
    pebbles-deploy sources and use that in Dockerfile instead of cloning
    a remote repo. Modify `branch-info` to read `.version_info` file in case
    the repository directory is not a git clone.

--- container-src/pebbles-deployer/build.bash
+++ container-src/pebbles-deployer/build.bash
@@ -12,4 +12,44 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IMAGE_AND_TAG=${1-cscfi/pebbles-deployer:latest}
 EXTRA_ARGS=${EXTRA_ARGS:-}
 
-$DOCKER_EXECUTABLE build --pull=true -t ${IMAGE_AND_TAG} ${EXTRA_ARGS} ${SCRIPT_DIR}
+# Create a temp directory for docker build context
+STAGE=$(mktemp -d)
+
+# Build tarball of the repo (only tracked files)
+(
+  # skip metadata on Mac
+  tar_args=""
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    tar_args="--no-xattr --no-mac-metadata"
+  fi
+
+  cd "$SCRIPT_DIR/../.."
+  git ls-files -z | tar --null $tar_args -czf "$STAGE/pebbles-deploy-src.tar.gz" -T -
+)
+
+# Add version file
+(
+  cd "$SCRIPT_DIR/../.."
+  COMMIT=$(git rev-parse HEAD)
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  DATE=$(git show -s --format=%cI HEAD)
+  NUM_DIRTY=$(( $(git status --porcelain | wc -l) ))
+  cat > "$STAGE/.version_info" <<EOF
+GIT_COMMIT=$COMMIT
+GIT_BRANCH=$BRANCH
+GIT_COMMIT_DATE=$DATE
+GIT_NUM_DIRTY=$NUM_DIRTY
+EOF
+)
+
+# Copy only the deployer Dockerfile + scripts into staging area
+cp -r "$SCRIPT_DIR/." "$STAGE/"
+
+$DOCKER_EXECUTABLE build \
+    --pull=true \
+    -t $IMAGE_AND_TAG \
+    -f "$STAGE/Dockerfile" \
+    ${EXTRA_ARGS} \
+    "$STAGE"
+
+rm -rf "$STAGE"
